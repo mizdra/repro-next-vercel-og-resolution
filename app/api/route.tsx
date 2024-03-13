afbc14c13c12764e5e47db9ffba0e1437d1c676b
mizdra <pp.mizdra@gmail.com>
2024-03-13 23:06:45 +0900

    first commit

---
+++ app/api/route.tsx
@@ -0,0 +1,30 @@
+import { ImageResponse } from "@vercel/og";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request: Request) {
+  const element = (
+    <div
+      style={{
+        fontSize: 40,
+        color: "black",
+        background: "white",
+        width: "100%",
+        height: "100%",
+        padding: "50px 200px",
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      👋 Hello
+    </div>
+  );
+  const response = new ImageResponse(element, { width: 1200, height: 630 });
+  const arrayBuffer = await response.arrayBuffer();
+  return new Response(arrayBuffer, {
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+}
